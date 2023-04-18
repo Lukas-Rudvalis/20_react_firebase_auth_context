a7d34d010674cd6b2a06ddc468cd66cf07302b74
Lukas Rudvalis <lukas.rudvalis@gmail.com>
2023-04-18 14:45:36 +0300

    use effect fix

--- src/components/comments/CommentsBlock.jsx
+++ src/components/comments/CommentsBlock.jsx
@@ -6,33 +6,32 @@ import { addDoc, collection, doc, getDocs, query } from 'firebase/firestore';
 import { db } from '../../firebase/firebase';
 
 function CommentsBlock({ postId }) {
+  const [commentsCollRef, setcommentsCollRef] = useState({});
   const [commentsArr, setCommentsArr] = useState([]);
+  const [commentTrigger, setCommentTrigger] = useState(false);
 
-  async function getCommentsAboutPost() {
-    const docRef = doc(db, 'posts', postId);
-    const commentsCollRef = collection(docRef, 'comments');
-    const q = query(commentsCollRef);
-    const querySnapshot = await getDocs(q);
-    const tempComments = [];
-    querySnapshot.forEach((doc) => {
-      tempComments.push({ uid: doc.id, ...doc.data() });
-      // console.log(doc.id, ' => ', doc.data());
-    });
-    setCommentsArr(tempComments);
-  }
   useEffect(() => {
+    async function getCommentsAboutPost() {
+      const docRef = doc(db, 'posts', postId);
+      const commentsCollRef = collection(docRef, 'comments'); // query
+      setcommentsCollRef(commentsCollRef);
+      const q = query(commentsCollRef);
+      const querySnapshot = await getDocs(q);
+      const tempComments = [];
+      querySnapshot.forEach((doc) => {
+        tempComments.push({ uid: doc.id, ...doc.data() });
+        // console.log(doc.id, ' => ', doc.data());
+      });
+      setCommentsArr(tempComments);
+    }
     getCommentsAboutPost();
-  }, []);
+  }, [commentTrigger]);
 
   async function createCommentFire(newCommentObj) {
     try {
-      const docRef = doc(db, 'posts', postId);
-      const commentsCollRef = await addDoc(
-        collection(docRef, 'comments'),
-        newCommentObj,
-      );
-      console.log('Document written with ID: ', commentsCollRef.id);
-      getCommentsAboutPost();
+      const result = await addDoc(commentsCollRef, newCommentObj);
+      console.log('Document written with ID: ', result.id);
+      setCommentTrigger(!commentTrigger);
     } catch (e) {
       console.error('Error adding document: ', e);
     }
